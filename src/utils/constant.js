282f2ff01ba3e0268f5aacf725c06e31f5a88152
Kaung Lynn Shann <153850401+KaungLynnShann@users.noreply.github.com>
2024-04-27 14:21:37 +0630

    Delete src directory

--- src/utils/constant.js
+++
@@ -1,2 +0,0 @@
-const localDomain = "http://localhost:8080";
-export default { localDomain };
--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-import constant from "./constant";
-import https from "./https";
-
-export default{
-    constant,https
-}
